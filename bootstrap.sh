7f062c85380f2e8696cc9d1f631dff9d47a776a4
cryptopool <cryptopool.builders@gmail.com>
2018-11-28 16:25:58 -0500

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,13 +7,13 @@
 #   curl https://raw.githubusercontent.com/cryptopool-builders/Multi-Pool-Installer/master/bootstrap.sh | bash
 #
 #########################################################
-if [ -z "$TAG" ]; then
-	TAG=v1.26
+if [ -z "${TAG}" ]; then
+	TAG=v1.27
 fi
 
 
 # Clone the MultiPool repository if it doesn't exist.
-if [ ! -d "$HOME"/multipool ]; then
+if [ ! -d $HOME/multipool ]; then
 	if [ ! -f /usr/bin/git ]; then
 		echo Installing git . . .
 		apt-get -q -q update
@@ -21,9 +21,9 @@ if [ ! -d "$HOME"/multipool ]; then
 		echo
 	fi
 
-	echo Downloading MultiPool Installer $TAG. . .
+	echo Downloading MultiPool Installer ${TAG}. . .
 	git clone \
-		-b $TAG --depth 1 \
+		-b ${TAG} --depth 1 \
 		https://github.com/cryptopool-builders/multipool_setup \
 		"$HOME"/multipool/install \
 		< /dev/null 2> /dev/null
@@ -32,14 +32,14 @@ if [ ! -d "$HOME"/multipool ]; then
 fi
 
 # Set permission and change directory to it.
-cd "$HOME"/multipool/install
+cd $HOME/multipool/install
 
 # Update it.
 sudo chown -R $USER $HOME/multipool/install/.git/
-if [ "$TAG" != `git describe --tags` ]; then
-	echo Updating MultiPool Installer to $TAG . . .
-	git fetch --depth 1 --force --prune origin tag $TAG
-	if ! git checkout -q $TAG; then
+if [ "${TAG}" != `git describe --tags` ]; then
+	echo Updating MultiPool Installer to ${TAG} . . .
+	git fetch --depth 1 --force --prune origin tag ${TAG}
+	if ! git checkout -q ${TAG}; then
 		echo "Update failed. Did you modify something in `pwd`?"
 		exit
 	fi
@@ -47,4 +47,4 @@ if [ "$TAG" != `git describe --tags` ]; then
 fi
 
 # Start setup script.
-bash "$HOME"/multipool/install/start.sh
+bash $HOME/multipool/install/start.sh
