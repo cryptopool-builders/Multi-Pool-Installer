4a86848c56db0cc8be92f7bd9e9bac147542130f
Todd McFarlane-Smith <emillium@gmail.com>
2021-08-04 14:04:43 +1000

    Get mltipool repo from emillium

--- bootstrap.sh
+++ bootstrap.sh
@@ -26,7 +26,7 @@ if [ ! -d $HOME/multipool ]; then
 	echo Downloading MultiPool Installer ${TAG}. . .
 	git clone \
 		-b ${TAG} --depth 1 \
-		https://github.com/cryptopool-builders/multipool_setup \
+		https://github.com/emillium/multipool_setup \
 		"$HOME"/multipool/install \
 		< /dev/null 2> /dev/null
 
